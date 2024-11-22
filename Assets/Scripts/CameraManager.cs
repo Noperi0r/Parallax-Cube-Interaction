baa16fdc5da0356e64a9c8e52c6784acf463a0a6
Noperi0r <mathewgwon@gmail.com>
2024-11-22 23:38:04 +0900

    init

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] List<Camera> _cams = new List<Camera>();
+
+    
+
+
+}
