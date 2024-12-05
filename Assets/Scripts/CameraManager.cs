3fb7cc038175b8852a292de485842e6464bebb2a
Noperi0r <mathewgwon@gmail.com>
2024-12-06 03:20:35 +0900

    feat: serial communication

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    [SerializeField] Transform _cubeCenter;
     [SerializeField] List<Camera> _cams = new List<Camera>();
 
-    
+    Vector3 _camToCenterVec;
+
+    void Start()
+    {
+        for(int i=0; i< transform.childCount; ++i)
+        {
+            Camera cam = transform.GetChild(i).GetComponent<Camera>();
+            if (cam)
+                _cams.Add(transform.GetChild(i).GetComponent<Camera>());
+        }
+    }
+
+    void Update()
+    {
+        //_camToCenterVec = _cubeCenter.position - transform.position;
+        //Quaternion.LookRotation(_camToCenterVec);
+    }
+
 
 
 }
